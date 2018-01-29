8c865e03616d959f3505fbe5aeadadd93575dc72
Shubham Bhindwal <shubhambhindwal123@gmail.com>
2018-01-29 14:27:08 +0530

    working code snippet

---
+++ AccountService/src/main/java/com/vomvos/AccountService/AccountServiceApplication.java
@@ -0,0 +1,27 @@
+package com.vomvos.AccountService;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
+
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().url("jdbc:mysql://192.168.99.100:3306/oauth2?useSSL=false")
+				.username("default").password("default").driverClassName("com.mysql.jdbc.Driver").build();
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # spring-microservices-demo
 This repository contain spring boot microservices snippet ..
+
+This is code is used to  build spring microservices application .
+
+Technologies :- 
+Spring-boot
+Edureka Discovery services 
+JPA
+Hibernate
+Spring-secutiry
+
+List of Services 
+Discovery Service 
+API-Gateway
+AuthServer(OAuth2)
+Account Service
